15517ac7b84cecac688e5fc5e360a5fa3cd54e89
Ahmed Lila <68854086+ahmedlila@users.noreply.github.com>
2023-06-26 06:43:11 +0300

    refactor 7

--- main.py
+++ main.py
@@ -14,6 +14,10 @@ import langchain
 from langchain.cache import InMemoryCache
 from dotenv import load_dotenv
 from helpers import * 
+import pvcheetah
+import pyaudio
+import argparse
+
 # load environment variables
 load_dotenv(dotenv_path="configs/.env", override=True, verbose=True)
 
@@ -24,9 +28,21 @@ langchain.llm_cache = InMemoryCache()
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 SONIOX_API_KEY = os.getenv("SONIOX_API_KEY")
 ELEVEN_API_KEY = os.getenv("ELEVEN_API_KEY")
+PICOVOICE_API_KEY = os.getenv("PICOVOICE_API_KEY")
 
 # define global variables
 agent_is_speaking = False
+
+
+# Picovoice initializations
+selected_microphone_index = 1
+
+CHANNELS = 1  # Mono
+RATE = 16000  # Sample rate expected by DeepSpeech model
+CHUNK_SIZE = 480  # Number of audio frames per buffer
+FORMAT = pyaudio.paInt16  # Audio format
+
+
 
 
 def play_agent_response(text: str, voice_id: str = "pNInz6obpgDQGcFmaJgB", model_id: str = "eleven_monolingual_v1", optimize_streaming_latency: int = 4):
@@ -74,57 +90,153 @@ def play_agent_response(text: str, voice_id: str = "pNInz6obpgDQGcFmaJgB", model
 
 
 
-def main():
-    # Initialize conversation
+def agent_speaks(sales_agent):
+    sales_agent.step()
+    agent_response = sales_agent.conversation_history[-1].replace('<END_OF_TURN>', '')
+    print("Agent response:", agent_response)
+    play_agent_response(agent_response)
+    return agent_response
+
+
+def transcribe_user_input(client):
+    final_words = []
+    print("Transcribing from your microphone...")
+
+    while agent_is_speaking:
+        time.sleep(0.001)
+
+    for result in transcribe_microphone(client):
+        new_final_words, non_final_words = split_words(result)
+        final_words += new_final_words
+        render_final_words(final_words)
+        render_non_final_words(non_final_words)
+
+        if len(final_words) > 2:
+            return " ".join(final_words).strip()
+
+    return ""
+
+
+def transcribe_audio_stream_using_cheetah(audio_chunk):
+    audio_data = np.frombuffer(audio_chunk, dtype=np.int16)
+
+    # Ensure the audio_data input is int16 and one-dimensional (mono)
+    assert audio_data.dtype == np.int16, f"Invalid data type: expected int16, got {audio_data.dtype}"
+    assert len(audio_data.shape) == 1, f"Invalid data shape: expected one-dimensional, but got {audio_data.shape}"
+
+    # Process audio data with Cheetah ASR
+    partial_transcript, is_endpoint = cheetah.process(audio_data)
+
+    return partial_transcript, is_endpoint
+
+def main(model_name = 'soniox'):
+    # initialize the agent
     llm = ChatOpenAI(temperature=0.9)
     sales_agent = SalesGPT.from_llm(llm)
 
-    # Initialize SpeechClient and transcriber
-    with SpeechClient() as client:
+    if model_name == 'soniox':
+        # Initialize SpeechClient and transcriber
+        with SpeechClient() as client:
+            count = 0
+            max_num_turns = 4
 
-        count = 0
-        max_num_turns = 4
+            while count != max_num_turns:
+                # Agent speaks
+                count += 1
+                agent_response = agent_speaks(sales_agent)
 
-        while count != max_num_turns:
-            # Agent speaks
-            count += 1
-            sales_agent.step()
-            agent_response = sales_agent.conversation_history[-1].replace('<END_OF_TURN>', '')
-            print("Agent response:", agent_response)
-            play_agent_response(agent_response)
+                # User speaks
+                user_input = transcribe_user_input(client)
+                if user_input:
+                    sales_agent.human_step(user_input)
+                    print(f"User input sent to agent: {user_input}")
 
-            # User speaks
-            final_words = []
-            print("Transcribing from your microphone...")
-
-            while agent_is_speaking:
-                time.sleep(0.001)
-
-            for result in transcribe_microphone(client):
-                new_final_words, non_final_words = split_words(result)
-                final_words += new_final_words
-                render_final_words(final_words)
-                render_non_final_words(non_final_words)
-
-
-                if len(final_words) > 2:
-                    sales_agent.human_step(" ".join(final_words).strip())
-                    print(f"User input sent to agent: {' '.join(final_words).strip()}")
+                if '<END_OF_CALL>' in agent_response:
+                    print('Sales Agent determined it is time to end the conversation.')
                     break
+    else:
+        # Create an initial Cheetah ASR object to get the frame length
+        cheetah = pvcheetah.create(access_key=PICOVOICE_API_KEY)
+        CHUNK_SIZE = cheetah.frame_length  # Use the frame length specified by Cheetah
+        cheetah = None  # Allow the Python garbage collector to clean up
 
-            if '<END_OF_CALL>' in agent_response:
-                print('Sales Agent determined it is time to end the conversation.')
-                break
+        audio = pyaudio.PyAudio()
+        stream = audio.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK_SIZE)
+
+        try:
+            count = 0
+            max_num_turns = 4
+
+            while count != max_num_turns:
+                count += 1
+                # Create a new Cheetah ASR object before each loop
+                cheetah = pvcheetah.create(access_key=PICOVOICE_API_KEY)
+
+                sales_agent.step()
+                agent_response = sales_agent.conversation_history[-1].replace('<END_OF_TURN>', '')
+                print("Agent response:", agent_response)
+                play_agent_response(agent_response)
+
+                print("Transcribing from your microphone...")
+                transcript = ""
+
+                while agent_is_speaking:
+                    time.sleep(0.001)
+
+                is_endpoint = False
+                start_time = time.time()
+                duration = 2  # Duration in seconds
+
+                while not is_endpoint and time.time() - start_time <= duration:
+                    buffer = stream.read(CHUNK_SIZE, exception_on_overflow=False)
+                    audio_data = np.frombuffer(buffer, dtype=np.int16)
+
+                    # Call the transcription function on the buffered audio
+                    partial_transcript, is_endpoint = transcribe_audio_stream_using_cheetah(audio_data)
+
+                    if partial_transcript:
+                        print(f"Transcribing: {partial_transcript}", end='\r')
+                        transcript += partial_transcript
+                        
+
+                # Send the transcribed text after the specified duration
+                sales_agent.human_step(transcript.strip())
+                print(f"User input sent to agent: {transcript.strip()}")
+
+                # Reset the transcript to an empty string at the end of the loop
+                transcript = ""
+
+                # Allow the Python garbage collector to clean up the Cheetah ASR object
+                cheetah = None
+
+                if '<END_OF_CALL>' in agent_response:
+                    print('Sales Agent determined it is time to end the conversation.')
+                    break
+        finally:
+            stream.stop_stream()
+            stream.close()
+            audio.terminate()
+
 
 
 
 if __name__ == "__main__":
+    # Initialize parser
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-m", "--model_name", default="soniox", help="name of the model you wantn to use")
+    args = parser.parse_args()
+
+    # Initialize Picovoice Cheetah ASR
+    if args.model_name =="picovoice":
+        cheetah = pvcheetah.create(access_key=PICOVOICE_API_KEY)
+        
     # Run the profiler
     profiler = cProfile.Profile()
     profiler.enable()
 
     # Call the function you want to profile
-    main()
+    main(model_name=args.model_name)
     
     # Stop the profiler
     profiler.disable()
