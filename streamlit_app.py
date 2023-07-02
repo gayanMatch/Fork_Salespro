232318c7c73c5dc293c14d22443eb031b4d3edfd
Ahmed Lila <68854086+ahmedlila@users.noreply.github.com>
2023-07-02 23:08:42 +0300

    update app soniox

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,18 @@
 import streamlit as st
 import sounddevice as sd
 import numpy as np
+import os 
+import soundfile as sf
+from soniox.transcribe_file import transcribe_file_short
+from soniox.speech_service import SpeechClient
+
+os.environ['SONIOX_API_KEY'] = "f7d0f5e9c111971168b9f9729048bc01ca16843a7fc50db9ca589d09b1c84318"
+SAMPLE_RATE = 8000
+DURATION = 3
+
 
 # Set up the Streamlit app
-st.title("Voice Recording App")
+st.title("Sales Agent Chatbot")
 
 # Function to record voice
 def record_voice():
@@ -22,9 +31,21 @@ def play_voice(recording):
     sd.play(recording)
     sd.wait()  # Wait until the playback is finished
 
+def main():
+    with SpeechClient() as client:
+        result = transcribe_file_short("recorded_audio.wav", client)
+        for word in result.words:
+            st.write(f"{word.text} {word.start_ms} {word.duration_ms}")
+
 # Create a button for recording voice
 if st.button("Record Voice"):
     st.write("Recording started. Speak into your microphone...")
-    recording = record_voice()
+    recorded_audio = record_voice()
     st.write("Recording finished. Here is your recorded voice:")
-    play_voice(recording)
+
+    # Save the recorded audio to a file
+    output_file = "recorded_audio.wav"
+    sf.write(output_file, recorded_audio, SAMPLE_RATE)   
+    main()
+    # play agent voice 
+    play_voice(recorded_audio)
